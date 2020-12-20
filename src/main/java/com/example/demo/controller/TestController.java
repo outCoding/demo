a8d8b8d815edd0dc140275664060992e58a2526c
outCoding <2511438208@qq.com>
2020-12-20 21:44:54 +0800

    fix

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,15 +1,22 @@
 package com.example.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@Slf4j
 public class TestController {
 
     @RequestMapping("test")
     public String test() {
-        String env = System.getenv("env");
-        return "env is :" + env;
+        Map<String, String> map = System.getenv();
+        for (String key : map.keySet()) {
+            log.info(key + "=" + map.get(key));
+        }
+        return "test";
     }
 
 
